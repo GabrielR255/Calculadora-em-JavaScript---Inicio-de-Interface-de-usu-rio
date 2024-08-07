77efc041cfe2e8b74b4a361d5396c48fd2c1e080
GabrielR255 <118090628+GabrielR255@users.noreply.github.com>
2024-08-07 16:43:16 -0300

    Add files via upload

---
+++ inter.js
@@ -0,0 +1,21 @@
+function calculate(){
+    var altura= document.getElementById("altura").value/100;
+    var peso= document.getElementById("peso").value;
+
+var imc=peso / altura ** 2;
+var text=""
+if (imc < 18.5) {
+    text="Você está seu peso Ideal"
+  } else if (imc < 24.9) {
+    text="Você está relativamente bem!"
+  } else if (imc < 29.9) {
+    text="Você está acima do peso médio"
+  } else if (imc < 39.9) {
+    text="Cuidado, você está com o seu peso alto!"
+  } else if (imc > 39.9) {
+    text="Gordo demais, feche a boca kkkkkk"
+  }
+
+  document.getElementById("minha_area").innerText=text
+
+}
